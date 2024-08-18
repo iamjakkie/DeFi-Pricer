35efbdff5e0560737cf0b504d760d7b130e93378
jakkie <jakubkielbasiewicz@gmail.com>
2024-08-18 12:26:56 +0200

    diesel db init

--- src/db/connection.rs
+++
@@ -1,5 +0,0 @@
-
-pub async fn connect_to_db() {
-    let (client, connection) = tokio_postgres::connect(
-        "url", tokio_postgres::NoTls).await.unwrap();
-}
--- src/db/mod.rs
+++
@@ -1,8 +0,0 @@
-// // mod.rs in db module
-//
-// // Re-export components of the db module
-// pub mod connection;
-// pub mod models;
-// pub mod operations;
-//
-// pub use connection::establish_connection;
